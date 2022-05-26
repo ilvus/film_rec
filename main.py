5b27da49236df0f68e9d53076a5d4a78c5729fee
Azamat <101277772+ilvus@users.noreply.github.com>
2022-05-26 14:08:21 +0600

    Update main.py
    Made the code more understandable, added a class and a function

--- main.py
+++ main.py
@@ -1,35 +1,54 @@
+import tkinter
 from unicodedata import name
 from bs4 import BeautifulSoup
 import requests
 from tkinter import *
 
-box = Tk()
-box.geometry("800x800")
-b = Button(box,text="sample")
-b.pack()
-box.mainloop()
 
-url = "https://www.timeout.com/film/best-movies-of-all-time"
-page = requests.get(url)
+class Info:
+    def __init__(self):
+        self.names = []
+        self.description = []
 
-names = []
-discription = []
+    def get_info(self):
+        url = "https://www.timeout.com/film/best-movies-of-all-time"
 
-soup = BeautifulSoup(page.text, "html.parser")
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, "html.parser")
 
-names = soup.findAll('h3', class_="_h3_cuogz_1")
+        self.names = soup.findAll('h3', class_="_h3_cuogz_1")
+        self.description = soup.findAll('div', class_="_summary_1p2xe_21")
 
-discription = soup.findAll('div', class_="_summary_1p2xe_21")
+    def get_names(self):
+        for i in self.names:
+            print(i.text)
+
+    def get_description(self):
+        for i in self.description:
+            print(i.text)
+
+    def window(self):
+        box = Tk()
+
+        box.geometry("500x500")
+
+        Text_box = Text(box, height= 15, width= 55)
+        for i in self.names[:100]:
+            label = Label(box, text=f"{i.text}")
+            label.config(font=("Courier", 14))
+        for g in self.description:
+            Fact = f"{g.text}"
+
+        label.pack()
+        Text_box.pack()
+
+        Text_box.insert(tkinter.END, Fact)
+        tkinter.mainloop()
 
 
-# for i in discription:
-#     print(i.text, end="\n")
-
-# for i in names:
-#     print(i.text)
-
-
-    
+A = Info()
+A.get_info()
+A.window()
 
 
 
