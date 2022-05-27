790fba50f9fb545fe14d28f22f4810b85f629779
Azamat <101277772+ilvus@users.noreply.github.com>
2022-05-28 01:01:50 +0600

    Update main.py
    I couldn't add buttons, I can't figure out how it works ;(

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import ttk
 from unicodedata import name
 from bs4 import BeautifulSoup
 import requests
@@ -9,6 +10,7 @@ class Info:
     def __init__(self):
         self.names = []
         self.description = []
+        self.c = 0
 
     def get_info(self):
         url = "https://www.timeout.com/film/best-movies-of-all-time"
@@ -32,23 +34,38 @@ class Info:
 
         box.geometry("500x500")
 
-        Text_box = Text(box, height= 15, width= 55)
-        for i in self.names[:100]:
-            label = Label(box, text=f"{i.text}")
-            label.config(font=("Courier", 14))
-        for g in self.description:
-            Fact = f"{g.text}"
+        Text_box = Text(box, height=15, width=55)
 
-        label.pack()
+        self.label = Label(box, text=f"{self.names[self.c].text}")
+        self.label.config(font=("Courier", 14))
+
+        self.Fact = f"{self.description[self.c].text}"
+
+        next_button = Button(box, command=self.next_clicked(), text="Next")
+        previous_button = Button(box, command=self.prev_clicked(), text="Prev")
+
+        previous_button.pack()
+        next_button.pack()
+        self.label.pack()
         Text_box.pack()
 
-        Text_box.insert(tkinter.END, Fact)
+        Text_box.insert(tkinter.END, self.Fact)
         tkinter.mainloop()
+
+    def next_clicked(self):
+        self.c += 1
+        self.label["text"] = self.names[self.c]
+
+
+
+    def prev_clicked(self):
+        if self.c < 0:
+            quit()
+        else:
+            self.c -= 1
+            self.Fact = self.description[self.c]
 
 
 A = Info()
 A.get_info()
 A.window()
-
-
-
